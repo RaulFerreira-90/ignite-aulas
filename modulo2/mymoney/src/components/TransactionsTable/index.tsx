3a57b5588383e9905b565fc0d3d260c863829c9c
Raul Ferreira <raul.oliveiraferreira@gmail.com>
2021-04-12 15:45:09 -0300

    adding sass

--- modulo2/mymoney/src/components/TransactionsTable/index.tsx
+++ modulo2/mymoney/src/components/TransactionsTable/index.tsx
@@ -22,10 +22,13 @@ export function TrasactionsTable() {
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
-                {new Intl.NumberFormat('pt-BR', {
+                {transaction.type === 'deposit' ? `${new Intl.NumberFormat('pt-BR', {
                   style: 'currency',
                   currency: 'BRL'
-                }).format(transaction.amount)}
+                }).format(transaction.amount)}` : `-${new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}`}
               </td>
               <td>{transaction.category}</td>
               <td>{new Intl.DateTimeFormat('pt-BR').format(
