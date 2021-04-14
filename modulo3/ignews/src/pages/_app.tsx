a6c597602090589baf96e11317551b69b97280ad
Raul Ferreira <raul.oliveiraferreira@gmail.com>
2021-04-13 21:07:54 -0300

    component signinbutton

--- modulo3/ignews/src/pages/_app.tsx
+++ modulo3/ignews/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { Header } from '../Header';
+import { Header } from '../components/Header';
 
 import '../styles/global.scss'
 
